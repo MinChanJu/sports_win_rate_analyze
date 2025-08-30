d7b52f431fc98a0eefff128bab2b2688738cb522
MinChanJu <mcj00220077@gmail.com>
2025-08-31 00:32:19 +0900

    feat: 데이터 decoder 시용하여 csv 셍성

--- crawler/file_checker.py
+++ crawler/file_checker.py
@@ -1,5 +1,6 @@
 import json, os, re
 
+
 def analyze_json_files(log_file_path, root_folder, start=1, end=270):
     total_summary = {}
     pattern = re.compile(r"S\d+G\d+N(\d+)\.json$")
@@ -11,7 +12,13 @@ def analyze_json_files(log_file_path, root_folder, start=1, end=270):
         if os.path.isdir(sub_path):
             check_log += f"======================================== [{name}] file check ========================================\n"
             files = os.listdir(sub_path)
-            files.sort(key=lambda x: (int(re.search(r'S\d+G\d+N(\d+)\.json$', x).group(1)) if re.search(r'S\d+G\d+N(\d+)\.json$', x) else float('inf')))
+            files.sort(
+                key=lambda x: (
+                    int(re.search(r"S\d+G\d+N(\d+)\.json$", x).group(1))
+                    if re.search(r"S\d+G\d+N(\d+)\.json$", x)
+                    else float("inf")
+                )
+            )
             count = len(files)
             wrong_files = []
             existing_files = set()
@@ -20,58 +27,85 @@ def analyze_json_files(log_file_path, root_folder, start=1, end=270):
                 if match:
                     existing_files.add(int(match.group(1)))
                     file_path = os.path.join(sub_path, file)
-                    with open(file_path, 'r') as f:
+                    with open(file_path, "r") as f:
                         content = f.read().strip()
                         json_content = json.loads(content)
                         check_log += f"{file}: {json_content['metainfo']['date']} ({json_content['metainfo']['home']['name']} vs {json_content['metainfo']['away']['name']})"
                         check_log += f" [{', '.join([f'{quarter}({len(json_content[quarter])})' for quarter in json_content['metainfo']['quarters']])}]"
-                        if ("Q1" in json_content and
-                            "Q2" in json_content and
-                            "Q3" in json_content and
-                            "Q4" in json_content):
+                        if (
+                            "Q1" in json_content
+                            and "Q2" in json_content
+                            and "Q3" in json_content
+                            and "Q4" in json_content
+                        ):
                             check_log += " ✅\n"
                         else:
                             check_log += " ❌\n"
-                            wrong_files.append("'" + '/'.join(json_content['metainfo']['url'].split('/')[-2:]) + "'")
+                            wrong_files.append(
+                                "'"
+                                + "/".join(
+                                    json_content["metainfo"]["url"].split("/")[-2:]
+                                )
+                                + "'"
+                            )
 
             # 빠진 번호 찾기
-            missing_files = [f'{files[0][:6]}N{i}' for i in range(start, end + 1) if i not in existing_files]
+            missing_files = [
+                f"{files[0][:6]}N{i}"
+                for i in range(start, end + 1)
+                if i not in existing_files
+            ]
 
             total_summary[name] = {
                 "count": count,
                 "wrong_files": wrong_files,
-                "missing_files": missing_files
+                "missing_files": missing_files,
             }
-            
+
             check_log += f"총 {count}개의 .json 파일\n"
             if wrong_files:
-                check_log += f"❌ 잘못된 파일 ({len(wrong_files)}): {', '.join(wrong_files)}\n"
+                check_log += (
+                    f"❌ 잘못된 파일 ({len(wrong_files)}): {', '.join(wrong_files)}\n"
+                )
             else:
                 check_log += "✅ 잘못된 파일 없음\n"
-            
+
             if missing_files:
-                check_log += f"❌ 빠진 번호 ({len(missing_files)}): {', '.join(missing_files)}\n"
+                check_log += (
+                    f"❌ 빠진 번호 ({len(missing_files)}): {', '.join(missing_files)}\n"
+                )
             else:
                 check_log += "✅ 빠진 번호 없음\n"
 
     check_log += f"======================================== kbl_data total summary ========================================\n"
-    check_log += f"총 합계: {sum(v['count'] for v in total_summary.values())}개의 .json 파일\n"
+    check_log += (
+        f"총 합계: {sum(v['count'] for v in total_summary.values())}개의 .json 파일\n"
+    )
     for k, v in total_summary.items():
-        if v['count']: check_log += f" - {k}: {v['count']}개\n"
+        if v["count"]:
+            check_log += f" - {k}: {v['count']}개\n"
     check_log += f"총 잘못된 파일: {sum(len(v['wrong_files']) for v in total_summary.values())}개\n"
     for k, v in total_summary.items():
-        if v['wrong_files']: check_log += f" - {k} ({len(v['wrong_files'])}): {', '.join(v['wrong_files'])}\n"
+        if v["wrong_files"]:
+            check_log += (
+                f" - {k} ({len(v['wrong_files'])}): {', '.join(v['wrong_files'])}\n"
+            )
     check_log += f"총 빠진 번호: {sum(len(v['missing_files']) for v in total_summary.values())}개\n"
     for k, v in total_summary.items():
-        if v['missing_files']: check_log += f" - {k} ({len(v['missing_files'])}): {', '.join(v['missing_files'])}\n"
+        if v["missing_files"]:
+            check_log += (
+                f" - {k} ({len(v['missing_files'])}): {', '.join(v['missing_files'])}\n"
+            )
     check_log += "================================================= copy =================================================\n"
     for k, v in total_summary.items():
-        if v['wrong_files']: check_log += f"'{k}': [{', '.join(v['wrong_files'])}],\n"
+        if v["wrong_files"]:
+            check_log += f"'{k}': [{', '.join(v['wrong_files'])}],\n"
     check_log += "========================================================================================================\n"
 
     with open(log_file_path, "w") as log_file:
         log_file.write(check_log)
         print(f"Log written to '{log_file_path}'")
+
 
 # 사용 예시
 root = "../kbl_data"
--- decoder/data_to_csv.py
+++ decoder/data_to_csv.py
@@ -1,61 +1,57 @@
-import copy, json, os, re
+import copy, os, re
 import pandas as pd
+from kbl_decoder import kbl_decoder
 
-def decoder(file_path):
-  stats = ['PTS', '2PM', '2PA', '2P%', '3PM', '3PA', '3P%', 'FGM', 'FGA', 'FG%', 'FTM', 'FTA', 'FT%', 'OREB', 'DREB', 'REB', 'AST', 'STL', 'BLK', 'GD', 'DK', 'TO', 'PF']
-  total_stats = {}
 
-  with open(file_path, 'r') as f:
-    data = json.load(f)
-    metainfo = data['metainfo']
-    home_players = metainfo['home']['players']
-    away_players = metainfo['away']['players']
-    quarters = metainfo['quarters']
-    for quarter in quarters:
-      total_stats[quarter] = {"home": {}, "away": {}}
-      for player in home_players:
-        total_stats[quarter]["home"][player] = {stat: 0 for stat in stats}
-      for player in away_players:
-        total_stats[quarter]["away"][player] = {stat: 0 for stat in stats}
+def data_to_csv(data_path: str, csv_path: str):
+    os.makedirs(csv_path, exist_ok=True)
+    s = 1
+    for folder in os.listdir(data_path):
+        if os.path.isdir(os.path.join(data_path, folder)):
+            files = os.listdir(os.path.join(data_path, folder))
+            files.sort(
+                key=lambda x: (
+                    int(re.search(r"S\d+G\d+N(\d+)\.json$", x).group(1))
+                    if re.search(r"S\d+G\d+N(\d+)\.json$", x)
+                    else float("inf")
+                )
+            )
+            records = []
+            for file in files:
+                if file.endswith(".json"):
+                    file_path = os.path.join(data_path, folder, file)
+                    total_stats, metainfo = kbl_decoder(file_path)
+                    row = {
+                        "gameKey": metainfo["gameKey"],
+                        "seasonName": metainfo["seasonName"],
+                        "date": metainfo["date"],
+                        "home_name": metainfo["home"]["name"],
+                        "away_name": metainfo["away"]["name"],
+                    }
 
-  return total_stats, metainfo
+                    for quarter in total_stats:
+                        quarter_row = copy.deepcopy(row)
+                        quarter_row["quarter"] = quarter
+                        for team in total_stats[quarter]:
+                            for player_idx, player in enumerate(
+                                total_stats[quarter][team]
+                            ):
+                                quarter_row[f"{team}_player_{player_idx+1}_name"] = (
+                                    player
+                                )
+                                for stat in total_stats[quarter][team][player]:
+                                    quarter_row[
+                                        f"{team}_player_{player_idx+1}_{stat}"
+                                    ] = total_stats[quarter][team][player][stat]
+                        records.append(quarter_row)
 
-def data_to_csv(data_path, csv_path):
-  os.makedirs(csv_path, exist_ok=True)
-  
-  for folder in os.listdir(data_path):
-    if os.path.isdir(os.path.join(data_path, folder)):
-      files = os.listdir(os.path.join(data_path, folder))
-      files.sort(key=lambda x: (int(re.search(r'S\d+G\d+N(\d+)\.json$', x).group(1)) if re.search(r'S\d+G\d+N(\d+)\.json$', x) else float('inf')))
-      records = []
-      for file in files:
-        if file.endswith('.json'):
-          file_path = os.path.join(data_path, folder, file)
-          total_stats, metainfo = decoder(file_path)
-          row = {
-              "gameKey": metainfo['gameKey'],
-              "seasonName": metainfo['seasonName'],
-              "date": metainfo['date'],
-              "home_name": metainfo['home']['name'],
-              "away_name": metainfo['away']['name'],
-          }
+            df = pd.DataFrame(records)
+            df.to_csv(f"{csv_path}/{folder}.csv", index=False, encoding="utf-8-sig")
+            print(f"{folder} 시즌")
+            print(f"    필드 개수 -> {len(df.columns)}개")
+            print(f"    데이터 개수 -> {len(records)}개")
+            print(f"    레코드 저장 완료 -> {csv_path}/{folder}.csv")
 
-          for quarter in total_stats:
-            quarter_row = copy.deepcopy(row)
-            quarter_row["quarter"] = quarter
-            for team in total_stats[quarter]:
-              for player_idx, player in enumerate(total_stats[quarter][team]):
-                quarter_row[f'{team}_player_{player_idx+1}_name'] = player
-                for stat in total_stats[quarter][team][player]:
-                  quarter_row[f'{team}_player_{player_idx+1}_{stat}'] = total_stats[quarter][team][player][stat]
-            records.append(quarter_row)
-              
-      df = pd.DataFrame(records)
-      df.to_csv(f"{csv_path}/{folder}.csv", index=False, encoding="utf-8-sig")
-      print(f"{folder} 시즌")
-      print(f"    필드 개수 -> {len(df.columns)}개")
-      print(f"    데이터 개수 -> {len(records)}개")
-      print(f"    레코드 저장 완료 -> {csv_path}/{folder}.csv")
 
 if __name__ == "__main__":
-  data_to_csv('../kbl_data', '../kbl_data_csv')
+    data_to_csv("../kbl_data", "../kbl_data_csv")
