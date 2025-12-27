827e156e3bdf399ca02ad8fb757921a20de872bf
MinChanJu <mcj00220077@gmail.com>
2025-12-27 19:52:02 +0900

    App 컴포넌트의 구조 개선 및 레이아웃 수정

--- packages/frontend/src/app.tsx
+++ packages/frontend/src/app.tsx
@@ -10,21 +10,23 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <PageTitle />
-      <Header />
+    <div className="w-[1500px]">
+      <BrowserRouter>
+        <PageTitle />
+        <Header />
 
-      <main>
-        <Routes>
-          <Route path={ROUTES.HOME} element={<HomePage />} />
-          <Route path={ROUTES.SEASON} element={<SeasonPage />} />
-          <Route path={ROUTES.GAME} element={<GamePage />} />
-          <Route path={ROUTES.NOT_FOUND} element={<Error />} />
-        </Routes>
-      </main>
+        <main>
+          <Routes>
+            <Route path={ROUTES.HOME} element={<HomePage />} />
+            <Route path={ROUTES.SEASON} element={<SeasonPage />} />
+            <Route path={ROUTES.GAME} element={<GamePage />} />
+            <Route path={ROUTES.NOT_FOUND} element={<Error />} />
+          </Routes>
+        </main>
 
-      <footer></footer>
-    </BrowserRouter>
+        <footer></footer>
+      </BrowserRouter>
+    </div>
   );
 };
 
